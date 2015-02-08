ecade2dccf4f9d730a55946f278546d96329ca8c
satarupaguha11 <satarupaguha11@gmail.com>
2015-02-09 03:31:12 +0530

    add emoticon & last senti word feature

--- code/generate_features.py
+++ code/generate_features.py
@@ -1,14 +1,17 @@
 from helper import *
 from nltk.util import ngrams
 from numpy import *
+import re
 bingliu_lexicon_dictionary = pickle.load(open('../data/lexicons/bingliu_lexicon_dictionary.pkl','r'))
+_regex_emoticon_happy = re.compile(r"((:)+(-)*(\))+)|((\()+(-)*(:)+)|<3|XD")
+_regex_emoticon_sad = re.compile(r"((:)+(-)*(\()+)")
 
 def compute_n_gram(sample,vocab,n):
 	feature_vector = zeros((len(vocab)),dtype="int32")
 	tokens = ngrams(sample,n)
 	#print tokens
 	for token in tokens:
-		if token in vocab:
+		#if token in vocab:
 			feature_vector[vocab[token]] = 1
 	return feature_vector
 def compute_manual_lexicons(sample):
@@ -17,7 +20,7 @@ def compute_manual_lexicons(sample):
 	num_pos_tokens = 0
 	num_neg_tokens = 0
 	for unigram in sample:
-		unigram = unigram.lower()
+		#unigram = unigram.lower()
 		if unigram in bingliu_lexicon_dictionary['positive']:
 			num_pos_tokens+=1
 		elif unigram in bingliu_lexicon_dictionary['negative']:
@@ -25,10 +28,38 @@ def compute_manual_lexicons(sample):
 	feature_vector[0]=num_pos_tokens
 	feature_vector[1]=num_neg_tokens
 	return feature_vector
-def generate_features(split,n,word_ngrams,manual_lexicons):
+def compute_emoticon_features(sample):
+	feature_vector = zeros((2),dtype="int32")
+	for token in sample:
+		if re.search(_regex_emoticon_happy, token):
+			#print token
+			feature_vector[0] = 1
+		elif re.search(_regex_emoticon_sad, token):
+			#print token
+			feature_vector[1] = 1
+	return feature_vector
+def compute_last_senti_word(sample):
+	feature_vector = zeros((1),dtype="int32")
+	flag = -1
+	for token in sample:
+		if token in bingliu_lexicon_dictionary['positive']:
+			flag = 0
+		elif token in bingliu_lexicon_dictionary['negative']:
+			flag = 1
+	if flag == 0:
+		feature_vector[0] = 1
+	elif flag == 1:
+		feature_vector[0] = -1
+	return feature_vector
+	
+def generate_features(split,n,word_ngrams,manual_lexicons,emoticons,last_senti_word):
 	n_features = 0
 	tokenized = load_pickle("../data/intermediate/"+split+"_tokenized_replaced.pkl")
 	n_samples = len(tokenized)
+	emoticon_features = array([])
+	manual_lexicon_features = array([])
+	ngram_features = array([])
+	last_senti_word_features = array([])
 	if word_ngrams == True:
 		if n==1:
 			vocab = load_pickle("../data/intermediate/unigram_dict.pkl")
@@ -41,6 +72,10 @@ def generate_features(split,n,word_ngrams,manual_lexicons):
 		n_features+=len(vocab)
 	if manual_lexicons == True:
 		n_features+=2
+	if emoticons == True:
+		n_features+=2
+	if last_senti_word == True:
+		n_features +=1
 	
 	feature_matrix = zeros((n_samples,n_features),dtype="int32")
 	
@@ -51,5 +86,9 @@ def generate_features(split,n,word_ngrams,manual_lexicons):
 			ngram_features = compute_n_gram(sample,vocab,n)
 		if manual_lexicons == True:
 			manual_lexicon_features = compute_manual_lexicons(sample)
-		feature_matrix[sample_no,:] = concatenate([ngram_features,manual_lexicon_features])
+		if emoticons == True:
+			emoticon_features = compute_emoticon_features(sample)
+		if last_senti_word == True:
+			last_senti_word_features = compute_last_senti_word(sample)
+		feature_matrix[sample_no,:] = concatenate([ngram_features,manual_lexicon_features,emoticon_features,last_senti_word_features])
 	return feature_matrix
